2746a144a0dc8dbc801168b19c2214b47f539889
Brandon <choccydigestive@me.com>
2023-12-03 22:25:02 +0000

    slightly faster again

--- d03.py
+++ d03.py
@@ -7,7 +7,7 @@ def check_neighbour(grid:list[str], x:int, y:int, value:int) -> bool:
     see if a neighbour is a symbol we're looking for
     """
     if x < 0 or x >= grid_width or y < 0 or y >= grid_height:
-        return False
+        return None
     neighbour = grid[y][x]
     if neighbour == '*':
         utils.dict_increment(stars, (x, y), [value])
@@ -29,10 +29,8 @@ for y, line in enumerate(grid):
     matches = re.finditer(r'\d+', line)
     for match in matches:
         value = int(match.group())
-        x1, x2 = match.span()
-        results = set()
-        results.add(check_neighbour(grid, x1 - 1, y, value)) # left
-        results.add(check_neighbour(grid, x2, y, value)) # right
+        x1, x2 = match.span() # x1 inclusive, x2 exclusive
+        results = {check_neighbour(grid, x1 - 1, y, value), check_neighbour(grid, x2, y, value)} # left and right
         for x in range(x1 - 1, x2 + 1):
             results.add(check_neighbour(grid, x, y - 1, value)) # row above
             results.add(check_neighbour(grid, x, y + 1, value)) # row below
