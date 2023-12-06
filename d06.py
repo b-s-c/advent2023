57e2cde7446eb1b64ca28eade92be124a704863f
Brandon <choccydigestive@me.com>
2023-12-06 17:44:49 +0000

    clean d6

--- d06.py
+++ d06.py
@@ -25,28 +25,18 @@ for race in range(0, len(time_p1)):
         travel_distance = 0
         travel_distance = time_to_move * hold_duration
         if travel_distance > distance_p1[race]:
-            #print("[race {}] hold duration: {}, travel distance: {}".format(race + 1, hold_duration, travel_distance))
             wins[race].append(hold_duration)
 
-#print(wins)
 p1_result = prod([len(x) for x in wins])
 print(p1_result)
 
-#print(time, distance)
-
-lower_bound = 0
-upper_bound = 0
-for hold_duration in range(0, time):
-    time_to_move = time - hold_duration
-    travel_distance = time_to_move * hold_duration
-    if travel_distance > distance:
-        lower_bound = hold_duration
-        break
-for hold_duration in range(time, -1, -1):
-    time_to_move = time - hold_duration
-    travel_distance = time_to_move * hold_duration
-    if travel_distance > distance:
-        upper_bound = hold_duration
-        break
-#print(lower_bound, upper_bound)
-print(upper_bound - lower_bound + 1) # p2
+solutions = []
+configs = ((0, time, 1), (time, -1, -1))
+for config in configs:
+    for hold_duration in range(*config):
+        time_to_move = time - hold_duration
+        travel_distance = time_to_move * hold_duration
+        if travel_distance > distance:
+            solutions.append(hold_duration)
+            break
+print(solutions[1] - solutions[0] + 1) # p2
