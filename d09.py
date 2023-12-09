b2b7014d2f4a8ce16c2ef0c728c468466c9407d8
Brandon <choccydigestive@me.com>
2023-12-09 13:45:22 +0000

    d09 clean + big

--- d09.py
+++ d09.py
@@ -1,7 +1,5 @@
-with open("input/09/real.txt") as f:
+with open("input/09/big.txt") as f:
     input = [list(map(int, line.strip().split())) for line in f.readlines()]
-
-print(input)
 
 def is_all_zero(seq: list[int]) -> bool:
     for item in seq:
@@ -19,39 +17,28 @@ def get_sequence_difference(seq: list[int]) -> list[int]:
         out.append(seq[i] - seq[i - 1])
     return out
 
-def extrapolate_forwards(seqs: list[list[int]]) -> list[list[int]]:
-    zeros = seqs[-1]
-    seqs[-1].append(0)
+def extrapolate(seqs: list[list[int]]) -> list[list[int]]:
+    seqs[-1].extend([0, 0])
     for seq_i in range(len(seqs) - 2, -1, -1):
         seq = seqs[seq_i]
-        value = seq[-1] + seqs[seq_i + 1][-1]
-        seq.append(value)
+        seq.append(seq[-1] + seqs[seq_i + 1][-1])
+        seq.insert(0, seq[0] - seqs[seq_i + 1][0])
     return seqs
 
-def extrapolate_backwards(seqs: list[list[int]]) -> list[list[int]]:
-    zeros = seqs[-1]
-    seqs[-1].insert(0, 0)
-    for seq_i in range(len(seqs) - 2, -1, -1):
-        seq = seqs[seq_i]
-        value = seq[0] - seqs[seq_i + 1][0]
-        seq.insert(0, value)
-    return seqs
-
-result = []
+full_sequences = []
 for line in input:
     current_result = [line]
     diff = line
     while not is_all_zero(diff):
         diff = get_sequence_difference(diff)
         current_result.append(diff)
-    result.append(current_result)
+    full_sequences.append(current_result)
 
 p1_total = 0
 p2_total = 0
-for i in range(0, len(result)):
-    result[i] = extrapolate_forwards(result[i])
-    p1_total += result[i][0][-1]
-    result[i] = extrapolate_backwards(result[i])
-    p2_total += result[i][0][0]
+for extrapolated_top_line in [extrapolate(fs)[0] for fs in full_sequences]:
+    p1_total += extrapolated_top_line[-1]
+    p2_total += extrapolated_top_line[0]
+
 print(p1_total)
 print(p2_total)
